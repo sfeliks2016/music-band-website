7ced656f4afd182c9f2baa0dca6d4326b0912ee1
Szymon Feliks <sz.feliks@gmail.com>
2017-05-18 16:45:00 +0200

    adding all project files

---
+++ playlist.js
@@ -0,0 +1,14 @@
+$(document).ready(function(){
+    
+
+var aud = document.getElementById("myAudio"); 
+
+function playAud() { 
+    aud.play(); 
+} 
+
+function pauseAud() { 
+    aud.pause(); 
+} 
+ 
+});
